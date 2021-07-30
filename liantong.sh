6cbf41f574941db57b4390ff0fdfafc3b43fccaa
ovenone <1162555170@qq.com>
2021-07-30 19:01:20 +0800

    Add files via upload

---
+++ liantong.sh
@@ -0,0 +1,7 @@
+termux-setup-storage
+mkdir liantong
+cd liantong
+pkg update
+pkg install git  nodejs-lts wget curl 
+git clone https://github.com/simo8102/chinaunicom-AutoSignMachine.git .
+npm install
